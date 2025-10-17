ab9919966b1064e5ce97daa69e5938066b7c26ec
Binhocanedo <fcanedo2001@gmail.com>
2025-10-17 06:56:35 -0300

    Endpoint Get All Users

--- demo/src/main/java/com/comuniSaface/demo/controller/UserController.java
+++ demo/src/main/java/com/comuniSaface/demo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.comuniSaface.demo.controller;
 import com.comuniSaface.demo.dto.UserDTO;
 import com.comuniSaface.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -16,6 +18,11 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @GetMapping
+    public ResponseEntity<Page<UserDTO>> findAll(Pageable pageable){
+        Page<UserDTO> dto = userService.findAll(pageable);
+        return ResponseEntity.ok(dto);
+    }
 
     @PostMapping
     public ResponseEntity<UserDTO> insert (@RequestBody UserDTO dto){
--- demo/src/main/java/com/comuniSaface/demo/service/UserService.java
+++ demo/src/main/java/com/comuniSaface/demo/service/UserService.java
@@ -1,8 +1,12 @@
 package com.comuniSaface.demo.service;
 
+import com.comuniSaface.demo.dto.PostDTO;
 import com.comuniSaface.demo.dto.UserDTO;
+import com.comuniSaface.demo.entities.PostEntity;
 import com.comuniSaface.demo.entities.UserEntity;
 import com.comuniSaface.demo.repositories.UserRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,6 +16,12 @@ public class UserService {
 
     public UserService(UserRepository userRepository){
         this.userRepository = userRepository;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<UserDTO> findAll(Pageable pageable){
+        Page<UserEntity> result = userRepository.findAll(pageable);
+        return result.map(UserDTO::new);
     }
 
 
