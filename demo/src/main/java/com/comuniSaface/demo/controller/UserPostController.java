c7bd4ea2b10bed0bf6ee6d620f83fa168adbe66e
Stefany Silva <seu-email@exemplo.com>
2025-11-14 20:54:05 -0300

    adição do endpoint para listar posts

---
+++ demo/src/main/java/com/comuniSaface/demo/controller/UserPostController.java
@@ -0,0 +1,40 @@
+package com.comuniSaface.demo.controller;
+
+import com.comuniSaface.demo.dto.PostDTO;
+import com.comuniSaface.demo.dto.UserDTO;
+import com.comuniSaface.demo.service.UserPostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import jakarta.servlet.http.HttpSession;
+
+@RestController
+@RequestMapping("/api")
+public class UserPostController {
+
+    private final UserPostService userPostService;
+
+    @Autowired
+    public UserPostController(UserPostService userPostService) {
+        this.userPostService = userPostService;
+    }
+
+    @GetMapping("/me/posts")
+    public ResponseEntity<Page<PostDTO>> myPosts(HttpSession session, Pageable pageable) {
+        Object obj = session.getAttribute("user");
+        if (obj == null) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        if (!(obj instanceof UserDTO)) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+        UserDTO user = (UserDTO) obj;
+        Page<PostDTO> page = userPostService.findPostsByUsuario(user.getId(), pageable);
+        return ResponseEntity.ok(page);
+    }
+
+}
+
